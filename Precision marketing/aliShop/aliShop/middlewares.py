81e867fb09a3c011274420fd051bd80ce765965c
zhonghongyi <1260132105@qq.com>
2019-08-13 20:15:13 +0800

    ..

--- Precision marketing/aliShop/aliShop/middlewares.py
+++ Precision marketing/aliShop/aliShop/middlewares.py
@@ -8,6 +8,8 @@
 from scrapy import signals
 import base64
 
+
+
 class AlishopSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
